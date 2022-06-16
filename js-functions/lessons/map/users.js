41c0e0ddceefee355a6f3731727c98667cdfaf8c
Petr Akulinsky <akulinsky@ya.ru>
2022-06-16 16:35:29 +0700

    add map task

---
+++ js-functions/lessons/map/users.js
@@ -0,0 +1,53 @@
+import getChildren from './users.js';
+
+export default (arr) => arr.map((child) => child.children).flat();
+
+const users = [
+  {
+    name: 'Tirion',
+    children: [{ name: 'Mira', birthday: '1983-03-23' }],
+  },
+
+  { name: 'Bronn', children: [] },
+
+  {
+    name: 'Sam',
+    children: [
+      { name: 'Aria', birthday: '2012-11-03' },
+      { name: 'Keit', birthday: '1933-05-14' },
+    ],
+  },
+
+  {
+    name: 'Rob',
+    children: [{ name: 'Tisha', birthday: '2012-11-03' }],
+  },
+];
+
+console.log(getChildren(users));
+// [
+//   { name: 'Mira', birthday: '1983-03-23' },
+//   { name: 'Aria', birthday: '2012-11-03' },
+//   { name: 'Keit', birthday: '1933-05-14' },
+//   { name: 'Tisha', birthday: '2012-11-03' },
+// ];
+
+
+
+users.js
+
+// Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход список пользователей и возвращает плоский список их детей. 
+// Дети каждого пользователя хранятся в виде массива в ключе children.
+
+// alternate
+// BEGIN
+// const getChildren = (users) => {
+//   const childrenOfUsers = users.map(({ children }) => children);
+//   return childrenOfUsers.flat();
+// };
+
+// export default getChildren;
+
+// Alternative solution using flatMap
+// export default (users) => users.flatMap(({ children }) => children);
+// END
